50e48036097f05cf48499b88d038cf9587fa1289
Furkan Sarıyar <sariyarfurkan@gmail.com>
2020-12-20 21:00:00 +0300

    token url is changed

--- src/main/java/co/sudoers/virtualnotes/configuration/SecurityConfig.java
+++ src/main/java/co/sudoers/virtualnotes/configuration/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
-                .antMatchers("/token", "/token/*").permitAll()
+                .antMatchers("/api/token", "/api/token/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
--- src/main/java/co/sudoers/virtualnotes/controller/AccountController.java
+++ src/main/java/co/sudoers/virtualnotes/controller/AccountController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.naming.AuthenticationException;
 
 @RestController
-@RequestMapping("/token")
+@RequestMapping("/api/token")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class AccountController {
 
